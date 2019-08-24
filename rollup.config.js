5117b88dfe310ee245c1cfacfd9bf4419bf47287
Markus Felten <Markus.Felten@gmx.de>
2019-08-24 21:54:41 +0200

    fix: use mf-styles css

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import copy from 'rollup-plugin-copy';
 
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
@@ -7,12 +6,13 @@ import json from "rollup-plugin-json";
 import { terser } from "rollup-plugin-terser";
 import autoPreprocess from "svelte-preprocess";
 import postcssImport from "postcss-import";
+import copy from "rollup-plugin-copy";
 
 import history from "connect-history-api-fallback";
 import proxy from "http-proxy-middleware";
 import express from "express";
 import { create as browserSyncFactory } from "browser-sync";
-import { config } from './package.json';
+import { config } from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
@@ -24,25 +24,33 @@ export default {
     format: "esm",
     file: `${dist}/bundle.mjs`
   },
-  plugins: [svelte({
-    dev: !production,
-
-    preprocess: autoPreprocess({
-      transformers: {
-        postcss: {
-          plugins: [postcssImport]
-        }
-      }
+  plugins: [
+    copy({
+      targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
     }),
-    css: css => css.write(`${dist}/bundle.css`)
-  }), resolve({ browser: true }), commonjs(), json({
-    preferConst: true,
-    compact: true
-  }), production && terser(), copy({
-    targets: [
-      { src: 'node_modules/mf-styling/global.css', dest: dist }
-    ]
-  })],
+    svelte({
+      dev: !production,
+
+      preprocess: autoPreprocess({
+        transformers: {
+          postcss: {
+            plugins: [postcssImport]
+          }
+        }
+      }),
+      css: css => css.write(`${dist}/bundle.css`)
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+    json({
+      preferConst: true,
+      compact: true
+    }),
+    production && terser(),
+    copy({
+      targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
+    })
+  ],
   watch: {
     clearScreen: false
   }
