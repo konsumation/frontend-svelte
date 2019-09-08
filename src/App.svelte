666cbdc9aed2eaa52de6632a620d18e7a574af69
Markus Felten <Markus.Felten@gmx.de>
2019-09-09 00:01:36 +0200

    fix(style): get rid of .wrapper

--- src/App.svelte
+++ src/App.svelte
@@ -8,42 +8,48 @@
   }
 </script>
 
-<div class="wrapper">
-  <nav>
-    <Link href="/">
-      <img class="logo" src="konsum.svg" alt="Konsum" />
-      <h2>Konsum</h2>
-    </Link>
+<nav>
+  <Link href="/">
+    <img class="logo" src="konsum.svg" alt="Konsum" />
+    <h2>Konsum</h2>
+  </Link>
 
-    <ul>
-      <li>
-        <Link href="/category"><h3>Categories</h3></Link>
-      </li>
-      <li>
-        <Link href="/insert"><h3>Insert</h3></Link>
-      </li>
-      <li>
-        <Link href="/about"><h3>About</h3></Link>
-      </li>
-      <li>
-        {#if $session.isValid}
-          <Menue>
-            <h3 slot="title" class="dropdown-trigger">{$session.username}</h3>
-            <ul class="dropdown-content">
-              <li>
-                <a href="#!" on:click|preventDefault={logout}>
-                  Logout {$session.username}
-                </a>
-              </li>
-            </ul>
-          </Menue>
-        {:else}
-          <Link href="/login"><h3>Login</h3></Link>
-        {/if}
-      </li>
-    </ul>
-  </nav>
-  <main>
-    <Outlet {router}>nothing there</Outlet>
-  </main>
-</div>
+  <ul>
+    <li>
+      <Link href="/category">
+        <h3>Categories</h3>
+      </Link>
+    </li>
+    <li>
+      <Link href="/insert">
+        <h3>Insert</h3>
+      </Link>
+    </li>
+    <li>
+      <Link href="/about">
+        <h3>About</h3>
+      </Link>
+    </li>
+    <li>
+      {#if $session.isValid}
+        <Menue>
+          <h3 slot="title" class="dropdown-trigger">{$session.username}</h3>
+          <ul class="dropdown-content">
+            <li>
+              <a href="#!" on:click|preventDefault={logout}>
+                Logout {$session.username}
+              </a>
+            </li>
+          </ul>
+        </Menue>
+      {:else}
+        <Link href="/login">
+          <h3>Login</h3>
+        </Link>
+      {/if}
+    </li>
+  </ul>
+</nav>
+<main>
+  <Outlet {router}>nothing there</Outlet>
+</main>
