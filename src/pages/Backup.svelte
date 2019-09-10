c1fb6dd4f6594c1b37f15bcbb37f38678ca7c90b
Markus Felten <Markus.Felten@gmx.de>
2019-09-10 13:29:47 +0200

    fix(backup): fetch with credentials

--- src/pages/Backup.svelte
+++ src/pages/Backup.svelte
@@ -1,7 +1,26 @@
 <script>
   import { config } from "../../package.json";
+  import { session } from "../main.mjs";
+
+  let active = false;
+
+  async function backup() {
+    active = true;
+    try {
+      await fetch(config.api + "/admin/backup", {
+        headers: session.authorizationHeader
+      });
+    } finally {
+      active = false;
+    }
+  }
 </script>
 
 <div>
-  <a href="{config.api}/admin/backup">Backup</a>
+  <button type="submit" on:click|once={backup}>
+    Backup
+    {#if active}
+      <div class="spinner" />
+    {/if}
+  </button>
 </div>
