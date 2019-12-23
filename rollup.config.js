79829af64cb6a0de4e1d8a915a0a93ad69a6586a
Markus Felten <Markus.Felten@gmx.de>
2019-12-23 19:31:57 +0100

    fix(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
-import consts from 'rollup-plugin-consts';
-import dev from "rollup-plugin-dev";
-import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
+
+import svelte from "rollup-plugin-svelte";
 import { terser } from "rollup-plugin-terser";
+import dev from "rollup-plugin-dev";
 import copy from "rollup-plugin-copy";
-import { name, version, description, config } from "./package.json";
+import consts from "rollup-plugin-consts";
+import { name, description, version, config } from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
@@ -18,26 +19,33 @@ export default {
     format: "esm",
     file: `${dist}/bundle.mjs`
   },
-  plugins: [consts({
-    name,
-    version,
-    description,
-    api: config.api,
-    base: config.base
-  }), copy({
-    targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
-  }), svelte({
-    dev: !production,
-    css: css => {
-      css.write(`${dist}/bundle.css`);
-    }
-  }), resolve({ browser: true }), commonjs(), production && terser(), dev({
-    port,
-    dirs: [dist],
-    spa: `${dist}/index.html`,
-    basePath: config.base,
-    proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
-  }),],
+  plugins: [
+    consts({
+      name,
+      version,
+      description,
+      ...config
+    }),
+    copy({
+      targets: [{ src: "node_modules/mf-styling/global.css", dest: dist }]
+    }),
+    svelte({
+      dev: !production,
+      css: css => {
+        css.write(`${dist}/bundle.css`);
+      }
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+    production && terser(),
+    dev({
+      port,
+      dirs: [dist],
+      spa: `${dist}/index.html`,
+      basePath: config.base,
+      proxy: { [`${config.api}/*`]: [config.proxyTarget, { https: true }] }
+    })
+  ],
   watch: {
     clearScreen: false
   }
