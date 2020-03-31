deeb112c561405220cb261b9abdfa8198dbea2e1
Markus Felten <Markus.Felten@gmx.de>
2020-03-31 22:54:24 +0200

    chore(rollup): cleanup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,4 @@
 import { readFileSync } from "fs";
-import fs from "fs";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
@@ -17,7 +16,7 @@ const port = 5000;
 
 export default () => {
   const { name, description, version, config } = JSON.parse(
-    fs.readFileSync("./package.json", { endoding: "utf8" })
+    readFileSync("./package.json", { endoding: "utf8" })
   );
 
   return {
