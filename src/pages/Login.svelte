4dd026517a82bac618ef3c509fa6f77e3abd25de
Markus Felten <Markus.Felten@gmx.de>
2020-06-11 18:57:56 +0200

    fix: use simplified transition continue after login redirection

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -2,16 +2,8 @@
   import { Login } from "svelte-session-manager";
   import { session, router } from "../main.mjs";
   import api from "consts:api";
-
-  async function result() {
-    const transition = router.transition;
-
-    if (transition) {
-      transition.continue();
-    }
-  }
 </script>
 
 <div class="center">
-  <Login {session} endpoint={api + '/authenticate'} {result} />
+  <Login {session} endpoint={api + '/authenticate'} result={() =>router.continue()} />
 </div>
