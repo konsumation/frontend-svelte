523f1b2749ce385fd4c17fa506011301d3c03ccd
Markus Felten <Markus.Felten@gmx.de>
2020-06-17 10:31:12 +0200

    feat(nginx): compress static content

--- pacman/hooks.sh
+++ pacman/hooks.sh
@@ -1,10 +1,19 @@
-
 post_install() {
+	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map)
 	systemctl reload nginx
 }
 
+pre_upgrade() {
+	(cd {{installdir}}; rm *.gz)
+}
+
 post_upgrade() {
+	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map)
 	systemctl reload nginx
+}
+
+pre_remove() {
+	(cd {{installdir}}; rm *.gz)
 }
 
 post_remove() {
