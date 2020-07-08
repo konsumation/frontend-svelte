5442abe73b187fd94a6e24fafcc47e412095eda0
Markus Felten <Markus.Felten@gmx.de>
2020-07-08 22:48:20 +0200

    fix: use router from outlet

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,9 +1,9 @@
 <script>
   import { Login } from "svelte-session-manager";
-  import { router } from "../main.mjs";
   import { session } from "../util.mjs";
-
   import api from "consts:api";
+
+  export let router;
 </script>
 
 <div class="center">
