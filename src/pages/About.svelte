5e6170ed58236f5d3a0528e04664b34e64680681
Markus Felten <Markus.Felten@gmx.de>
2020-09-16 20:33:27 +0200

    test: fix import

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,13 +1,5 @@
 <script>
-  import {
-    DateTime,
-    Duration,
-    dateFormatter,
-    formatDuration,
-    formatBytes,
-    About,
-    SessionDetails
-  } from "svelte-common";
+  import { About, SessionDetails, ServerDetails } from "svelte-common";
   import { state } from "../main.mjs";
   import { session } from "../util.mjs";
   import name from "consts:name";
@@ -16,23 +8,6 @@
 </script>
 
 <About {name} {version} {description}>
-  <tr>
-    <td>Server Version</td>
-    <td>{$state.version}</td>
-  </tr>
-  <tr>
-    <td>Server Uptime</td>
-    <td>
-      <Duration seconds={$state.uptime} />
-    </td>
-  </tr>
-  <tr>
-    <td>Server Heap Total</td>
-    <td>{formatBytes($state.memory.heapTotal)}</td>
-  </tr>
-  <tr>
-    <td>Server Heap Used</td>
-    <td>{formatBytes($state.memory.heapUsed)}</td>
-  </tr>
+  <ServerDetails server={$state} />
   <SessionDetails session={$session} />
 </About>
