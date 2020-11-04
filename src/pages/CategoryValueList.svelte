63856eb44c9acdc6836df68a70054a69ab78b747
Markus Felten <Markus.Felten@gmx.de>
2020-11-04 19:42:39 +0100

    feat: scroll to last if fragment last is given

--- src/pages/CategoryValueList.svelte
+++ src/pages/CategoryValueList.svelte
@@ -1,31 +1,41 @@
 <script>
-  import { Link } from "svelte-guard-history-router";
   import { DateTime } from "svelte-common";
 
   export let router;
-  
+
   const route = router.route;
-  
+
   function time2Date(time) {
     const date = new Date();
     date.setTime(time * 1000);
     return date;
   }
+
+  setTimeout(() => {
+    if (router.path.match(/#last/)) {
+      const last = document.getElementById("last");
+      if (last) {
+        last.scrollIntoView();
+      }
+    }
+  }, 1000);
 </script>
 
 {#if $route}
-<table class="bordered striped hoverable">
-  <thead>
-    <th>Date</th>
-    <th>Value</th>
-  </thead>
-  <tbody>
-    {#each $route as entry, i}
-      <tr id={i === 0 ? 'first' : i === $route.length - 1 ? 'last' : ''}>
-        <td><DateTime date={time2Date(entry.time)}/></td>
-        <td>{entry.value}</td>
-      </tr>
-    {/each}
-  </tbody>
-</table>
+  <table class="bordered striped hoverable">
+    <thead>
+      <th>Date</th>
+      <th>Value</th>
+    </thead>
+    <tbody>
+      {#each $route as entry, i}
+        <tr id={i === 0 ? 'first' : i === $route.length - 1 ? 'last' : ''}>
+          <td>
+            <DateTime date={time2Date(entry.time)} />
+          </td>
+          <td>{entry.value}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 {/if}
