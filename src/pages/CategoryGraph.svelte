beef6fadaf9027d70b744737932d637bbbb15771
Markus Felten <Markus.Felten@gmx.de>
2020-12-05 20:05:47 +0100

    fix: use layercacke

--- src/pages/CategoryGraph.svelte
+++ src/pages/CategoryGraph.svelte
@@ -1,40 +1,60 @@
 <script>
-  import { TimeSeries, DataSet } from "svelte-time-series";
+  import { LayerCake, Svg } from "layercake";
+  import Line from "../components/Line.svelte";
+  import Area from "../components/Area.svelte";
+  import AxisX from "../components/AxisX.svelte";
+  import AxisY from "../components/AxisY.svelte";
 
   export let router;
-  
   const route = router.route;
-  
-  const padding = { top: 20, right: 15, bottom: 22, left: 25 };
-  const yTicks = [0, 5, 10, 16, 20];
-  const xTicks = [941673600, 1566467076];
-  let points = [{ x: 941673600, y: 0 }];
+
+  let data = [
+  ];
 
   $: {
     const vv = $route.value;
 
     if (vv) {
-      points = [];
       let last = vv[0];
 
+      data = [];
       for (const c of vv) {
         const days = (c.time - last.time) / (24 * 60 * 60);
         const y = (c.value - last.value) / days;
 
-        if (c.time > 0 && y >= 0) {
-          points.push({ x: c.time, y });
+        if (c.time > 0 && y >= 0 && y < 30) {
+          const x = (c.time / (364.25 * 24 * 60 * 60)) + 1970
+          data.push({ x, y });
+        }
+        else {
+          console.log(c.time / (364.25 * 24 * 60 * 60) + 1970);
         }
         last = c;
       }
     }
   }
-
-  const width = 1500;
-  const height = 300;
+  
 </script>
 
-{#if $route && points.length}
-  <TimeSeries {padding} {width} {height} {xTicks} {yTicks} {points}>
-    <DataSet {padding} {width} {height} {xTicks} {yTicks} {points} />
-  </TimeSeries>
-{:else}No such category{/if}
+<style>
+  .chart-container {
+    width: 1000px;
+    height: 200px;
+  }
+</style>
+
+<div class="chart-container">
+  <LayerCake
+    padding={{ right: 10, bottom: 20, left: 25 }}
+    x={'x'}
+    y={'y'}
+    yDomain={[0, null]}
+    {data}>
+    <Svg>
+      <AxisX />
+      <AxisY ticks={4} />
+      <Line />
+      <Area />
+    </Svg>
+  </LayerCake>
+</div>
