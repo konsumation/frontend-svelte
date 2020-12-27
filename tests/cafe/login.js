84f60374e3313a3b1cb81a26ae2abb5c4cf32e62
Markus Felten <Markus.Felten@gmx.de>
2020-12-27 18:15:02 +0100

    chore: prepare testing with testcafe

---
+++ tests/cafe/login.js
@@ -0,0 +1,13 @@
+import { Selector } from "testcafe";
+
+const base = "http://localhost:5000";
+
+fixture`login`.page`${base}/index.html`;
+
+test("correct credentials", async t => {
+  await t
+    .typeText("#username", "user")
+    .typeText("#password", "secret")
+    .click("#submit");
+  await t.expect(Selector("#session_username").innerText).eql("user");
+});
