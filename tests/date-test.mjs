44b23c1a3d94e444c137f67c943a7cc9d7c93b58
Markus Felten <Markus.Felten@gmx.de>
2020-12-30 17:10:20 +0100

    test: use ava macro

--- tests/date-test.mjs
+++ tests/date-test.mjs
@@ -1,6 +1,11 @@
 import test from "ava";
 import { parseDate } from "../src/date.mjs";
 
-test("date", t => {
-  t.deepEqual(parseDate("8.9.2019, 19:38:34"), new Date("9.8.2019, 19:38:34"));
-});
+function dt(t, a, b) {
+  t.deepEqual(parseDate(a), new Date(b));
+}
+
+dt.title = (providedTitle = "parseDate", a, b) =>
+  `${providedTitle} ${a}`.trim();
+
+test(dt, "8.9.2019, 19:38:34", "9.8.2019, 19:38:34");
