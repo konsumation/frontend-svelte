c531c7bc52583b837ba7aa72124f51b23a5bc603
Markus Felten <Markus.Felten@gmx.de>
2020-12-30 17:16:16 +0100

    fix: remove unused code

--- src/main.mjs
+++ src/main.mjs
@@ -1,12 +1,6 @@
 import {} from "./service-worker/registration.mjs";
 import App from "./App.svelte";
 
-export const now = readable(new Date(), set => {
-  const interval = setInterval(() => set(new Date()), 1000);
-  return () => clearInterval(interval);
-});
-
-
 export default new App({
   target: document.body
 });
