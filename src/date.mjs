88ee603731b1f2ca21b25b2b66b105e7e31bab4e
k0nsti <kkarpman@clickforit.de>
2021-01-02 12:17:21 +0100

    fix: date could be without seconds

--- src/date.mjs
+++ src/date.mjs
@@ -2,7 +2,7 @@ export function parseDate(str) {
 
   // yyyy-MM-ddThh:mm:ss
   // 2017-06-01T08:30:00
-  let m = str.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/);
+  let m = str.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):?(\d{2})?/);
   if (m) {
     const date = new Date();
     date.setFullYear(parseInt(m[1], 10));
@@ -11,13 +11,13 @@ export function parseDate(str) {
 
     date.setHours(parseInt(m[4], 10));
     date.setMinutes(parseInt(m[5], 10));
-    date.setSeconds(parseInt(m[6], 10));
+    date.setSeconds(parseInt(m[6]==undefined ? "00" : m[6], 10)); //seconds could be empty
     date.setMilliseconds(0);
     return date;
   }
 
   //8.9.2019, 19:38:34
-  m = str.match(/(\d+)\.(\d+)\.(\d+)[\s,]+(\d+):(\d+):(\d+)/);
+  m = str.match(/(\d+)\.(\d+)\.(\d+)[\s,]+(\d+):(\d+):?(\d+)?/);
   if (m) {
     const date = new Date();
 
@@ -27,7 +27,7 @@ export function parseDate(str) {
 
     date.setHours(parseInt(m[4], 10));
     date.setMinutes(parseInt(m[5], 10));
-    date.setSeconds(parseInt(m[6], 10));
+    date.setSeconds(parseInt(m[6]==undefined ? "00" : m[6], 10)); //seconds could be empty
     date.setMilliseconds(0);
     return date;
   }
