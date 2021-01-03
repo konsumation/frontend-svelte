7a99a94b762bb97080fde88e8648c02d886d4ec9
Markus Felten <Markus.Felten@gmx.de>
2021-01-03 14:49:21 +0100

    fix: handle response without headers

--- src/handle-failed-response.mjs
+++ src/handle-failed-response.mjs
@@ -4,10 +4,11 @@
  * @return {string}
  */
 export async function handleFailedResponse(response) {
-    let message = response.statusText;
-  
+  let message = response.statusText;
+
+  if (response.headers) {
     const ct = response.headers.get("Content-Type").replace(/;.*/, "");
-  
+
     switch (ct) {
       case "text/plain":
         message += "\n" + (await response.text());
@@ -15,7 +16,7 @@ export async function handleFailedResponse(response) {
       case "text/html":
         const root = document.createElement("html");
         root.innerHTML = await response.text();
-  
+
         for (const tag of ["title", "h1", "h2"]) {
           for (const item of root.getElementsByTagName(tag)) {
             const text = item.innerText;
@@ -26,10 +27,10 @@ export async function handleFailedResponse(response) {
         }
         break;
     }
-  
-    console.log(message);
-    alert(message);
-
-    return message;
   }
-  
+
+  console.log(message);
+  alert(message);
+
+  return message;
+}
