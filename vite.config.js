4c7704ada326d9033c5f9171e24f51744ac729ff
k0nsti <kkarpman@clickforit.de>
2021-08-24 16:10:58 +0200

    feat: add vite files

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://localhost:12345",
+        rewrite: (path) => path.replace(/^\/api/,"")
+      }
+    }
+  },
+  define: {
+    "api": JSON.stringify("/services/konsum/api"),
+    "base": JSON.stringify("/services/konsum"),
+    "proxyTarget": JSON.stringify("http://localhost:12345"),
+    "title": JSON.stringify("Konsum"),
+    "version" : JSON.stringify("1.2.3")
+  }
+})
