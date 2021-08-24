e01b112be9e509764a6fa6a9da9353bf72b65972
Markus Felten <Markus.Felten@gmx.de>
2021-08-24 18:30:24 +0200

    fix: back to full App

--- src/App.svelte
+++ src/App.svelte
@@ -22,11 +22,12 @@
   import CategoryGraph from "./pages/CategoryGraph.svelte";
   import { Category, categoryIterator, valueIterator } from "./category.mjs";
   import { session } from "./util.mjs";
+  import { base } from "./constants.mjs";
 
   const enshureSession = redirectGuard("/login", () => !session.isValid);
 </script>
 
-<Router {import.meta.env.BASE_URL}>
+<Router {base}>
   <TopNav offset={42}>
     <Route href="/" path="*" component={Home}>
       <img class="logo" src="images/icon.svg" alt="Konsum" />
@@ -40,26 +41,28 @@
           iteratorFor={categoryIterator}
           objectInstance={Category}
           guard={enshureSession}
-          component={CategoriesPage}>
+          component={CategoriesPage}
+        >
           Categories
-          <Route
-            path="/add"
-            component={AddCategoryPage} />
+          <Route path="/add" component={AddCategoryPage} />
           <Route
             path="/:category"
-            propertyMapping={{ category: 'name' }}
+            propertyMapping={{ category: "name" }}
             factory={DetailRoute}
             linkComponent={NamedObjectLink}
-            component={CategoryPage}>
+            component={CategoryPage}
+          >
             <Route path="/values" iteratorFor={valueIterator}>
               <Route
                 path="/list"
                 factory={MasterRoute}
-                component={CategoryValueList} />
+                component={CategoryValueList}
+              />
               <Route
                 path="/graph"
                 factory={MasterRoute}
-                component={CategoryGraph} />
+                component={CategoryGraph}
+              />
             </Route>
           </Route>
         </Route>
@@ -70,7 +73,8 @@
           factory={MasterRoute}
           iteratorFor={categoryIterator}
           guard={enshureSession}
-          component={Insert}>
+          component={Insert}
+        >
           Insert
         </Route>
       </li>
@@ -87,7 +91,8 @@
               <a
                 href="/"
                 class="dropdown-item"
-                on:click|preventDefault={() => session.invalidate()}>
+                on:click|preventDefault={() => session.invalidate()}
+              >
                 Logout
                 {$session.username}
               </a>
@@ -96,7 +101,8 @@
                 path="/admin"
                 guard={enshureSession}
                 class="dropdown-item"
-                component={Admin}>
+                component={Admin}
+              >
                 Admin
               </Route>
             </div>
--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,5 @@
 import {} from "./service-worker/registration.mjs";
-import App from "./App1.svelte";
+import App from "./App.svelte";
 
 export default new App({
   target: document.body
