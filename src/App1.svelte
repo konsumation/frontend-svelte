a5f195a4d46ff23608a9f16f6c91bc8b4d3ef2b4
Markus Felten <Markus.Felten@gmx.de>
2021-08-24 17:17:37 +0200

    refactor: reorg files

---
+++ src/App1.svelte
@@ -0,0 +1,8 @@
+<script>
+  import * as style from "./main.css";
+</script>
+
+  <h1>
+      Hallo
+  </h1>
+
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "src",
+  base: "/services/konsum/",
+
   plugins: [svelte()],
+
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "http://localhost:12345",
-        rewrite: (path) => path.replace(/^\/api/,"")
+        rewrite: path => path.replace(/^\/api/, "")
       }
     }
-  },
-  define: {
-    "api": JSON.stringify("/services/konsum/api"),
-    "base": JSON.stringify("/services/konsum"),
-    "proxyTarget": JSON.stringify("http://localhost:12345"),
-    "title": JSON.stringify("Konsum"),
-    "version" : JSON.stringify("1.2.3")
   }
-})
+  
+  ,
+  define: {
+    api: JSON.stringify("/services/konsum/api"),
+    base: JSON.stringify("/services/konsum"),
+    proxyTarget: JSON.stringify("http://localhost:12345"),
+    title: JSON.stringify("Konsum"),
+    version: JSON.stringify("1.2.3")
+  }
+  
+});
