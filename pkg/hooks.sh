29a51e26b64e780587864b5196fa9c0fde492e86
Markus Felten <Markus.Felten@gmx.de>
2022-02-02 22:44:15 +0100

    fix: only run compressors if actual directory exists

--- pkg/hooks.sh
+++ pkg/hooks.sh
@@ -1,20 +1,20 @@
 
 post_install() {
-	(cd {{installdir}}; find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
+	(cd {{installdir}} && find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
 	systemctl reload nginx
 }
 
 pre_upgrade() {
-	(cd {{installdir}}; find -type f -name "*.gz" -print0|xargs -0 rm)
+	(cd {{installdir}} && find -type f -name "*.gz" -print0|xargs -0 rm)
 }
 
 post_upgrade() {
-	(cd {{installdir}}; find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
+	(cd {{installdir}} && find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
 	systemctl reload nginx
 }
 
 pre_remove() {
-	(cd {{installdir}}; find -type f -name "*.gz" -print0|xargs -0 rm)
+	(cd {{installdir}} && find -type f -name "*.gz" -print0|xargs -0 rm)
 }
 
 post_remove() {
