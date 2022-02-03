b9d5861f8e6cb21d37f2ad59413d9572b86bbc35
Markus Felten <Markus.Felten@gmx.de>
2022-02-03 21:07:00 +0100

    chore: cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { readFile } from "fs/promises";
 import { mkdirSync, readFileSync } from "fs";
 import { execFile } from "child_process";
-
-import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+const encodingOptions = { encoding: "utf8" };
+
 export default defineConfig(async ({ command, mode }) => {
   const pkg = JSON.parse(
-    await readFile(new URL("package.json", import.meta.url).pathname, {
-      encoding: "utf8"
-    })
+    await readFile(
+      new URL("package.json", import.meta.url).pathname,
+      encodingOptions
+    )
   );
 
   const production = mode === "production";
@@ -20,17 +21,22 @@ export default defineConfig(async ({ command, mode }) => {
     mkdirSync("build/db", { recursive: true });
     const konsum = execFile(
       "node",
-      ["node_modules/@konsumation/konsum/src/konsum-cli.mjs", "-c", "tests/config", "start"],
+      [
+        "node_modules/@konsumation/konsum/src/konsum-cli.mjs",
+        "-c",
+        "tests/config",
+        "start"
+      ],
       (error, stdout, stderr) => console.log(error, stdout, stderr)
     );
-  
+
     const { http } = JSON.parse(
-      readFileSync("tests/config/config.json", { endoding: "utf8" })
+      readFileSync("tests/config/config.json", encodingOptions)
     );
-  
+
     target = `http://localhost:${http.port}/`;
   }
-  
+
   const base = "/services/konsum/";
   const api = `${base}api`;
 
@@ -42,7 +48,7 @@ export default defineConfig(async ({ command, mode }) => {
   return {
     root: "src",
     base,
-    
+
     plugins: [
       svelte({
         compilerOptions: {
