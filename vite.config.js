3a9920c6e438cbe9a2e71a4f4a8f3bed89d61b7a
Markus Felten <Markus.Felten@gmx.de>
2022-04-16 20:52:16 +0200

    fix: use npm mf-styling

--- vite.config.js
+++ vite.config.js
@@ -37,8 +37,8 @@ export default defineConfig(async ({ command, mode }) => {
     target = `http://localhost:${http.port}/`;
   }
 
-  const base = "/services/konsum/";
-  const api = `${base}api`;
+  const base = pkg.pkg['http.base.path'];
+  const api = pkg.pkg['http.api.path'];
 
   process.env["VITE_API"] = api;
   process.env["VITE_NAME"] = pkg.name;
