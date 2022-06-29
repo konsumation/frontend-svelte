2444187303595acdf523fe68caf289fe55777b19
Markus Felten <Markus.Felten@gmx.de>
2022-06-29 02:04:19 +0200

    chore: nginx restart doen by aplm

--- pkg/hooks.sh
+++ pkg/hooks.sh
@@ -1,7 +1,6 @@
 
 post_install() {
 	(cd {{install.dir}} && find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
-	systemctl reload nginx
 }
 
 pre_upgrade() {
@@ -10,13 +9,8 @@ pre_upgrade() {
 
 post_upgrade() {
 	(cd {{install.dir}} && find -type f ! -name "*.gz" -print0|xargs -0 gzip -k -9)
-	systemctl reload nginx
 }
 
 pre_remove() {
 	(cd {{install.dir}} && find -type f -name "*.gz" -print0|xargs -0 rm)
-}
-
-post_remove() {
-	systemctl reload nginx
 }
