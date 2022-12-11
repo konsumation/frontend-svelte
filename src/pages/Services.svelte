d2d2b8782405de4f77c3f0a3b658c0422c2a152f
Markus Felten <Markus.Felten@gmx.de>
2022-12-11 12:41:40 +0100

    add missing src/pages/Services.svelte from template

---
+++ src/pages/Services.svelte
@@ -0,0 +1,17 @@
+<script>
+  import {
+    ServiceProvider,
+    ServiceCanvas
+  } from "@kronos-integration/svelte-components";
+  import { websocketStore } from "svelte-websocket-store";
+  import { session } from "../session.mjs";
+  import { api_ws } from "../constants.mjs";
+
+  const protocols = ["access_token", session.access_token];
+  const services = new ServiceProvider(
+    websocketStore(api_ws + "/admin/services", {}, protocols),
+    websocketStore(api_ws + "/admin/requests", {}, protocols)
+  );
+</script>
+
+<ServiceCanvas {services} />
