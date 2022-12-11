dbf3fd5d20fd16e905c3ef37cdf133612b75682d
Markus Felten <Markus.Felten@gmx.de>
2022-12-11 20:28:37 +0100

    Revert "add missing src/service-worker/registration.mjs from template"
    This reverts commit 2c4996c7e078c286da1c9ef3e0d713a562c46c4a.

--- src/service-worker/registration.mjs
+++
@@ -1,4 +0,0 @@
-import { initializeServiceWorker } from "svelte-common";
-
-const { serviceWorker, serviceWorkerRegistration } = initializeServiceWorker("bundle.service-worker.mjs");
-export { serviceWorker, serviceWorkerRegistration };
