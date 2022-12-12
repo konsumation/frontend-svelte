06bbef9cc0e274efdb68e0e42f437cacea71e7fe
Markus Felten <Markus.Felten@gmx.de>
2022-12-12 10:27:16 +0100

    add missing src/pages/UsersPage.svelte from template

---
+++ src/pages/UsersPage.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { ObjectLink } from "svelte-guard-history-router";
+
+  export let router;
+
+  const route = router.route;
+  const users = $route.value;
+</script>
+
+<ul>
+  {#each users as user}
+    <li>
+      <ObjectLink object={user} />
+    </li>
+  {/each}
+</ul>
