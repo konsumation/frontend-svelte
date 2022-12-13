3d87cdadc336830afb5c15e4f38b0365366a3aab
Markus Felten <Markus.Felten@gmx.de>
2022-12-13 10:31:20 +0100

    add missing src/pages/UserPage.svelte from template

---
+++ src/pages/UserPage.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { ObjectLink } from "svelte-guard-history-router";
+  export let router;
+
+  const route = router.route;
+  const user = $route.value;
+</script>
+
+<h1>{user.cn}</h1>
+
+<table>
+  {#each Object.entries(user) as kv}
+    <tr>
+      <td>{kv[0]}</td>
+      <td>{kv[1]}</td>
+    </tr>
+  {/each}
+</table>
+
+<ObjectLink object={user} suffix="/entitlements">Entitlements</ObjectLink>
