3cfcd85f18f28f4ae7706ce0d312d4c0c01a1fc0
Markus Felten <Markus.Felten@gmx.de>
2022-12-14 16:55:02 +0100

    add missing src/EntitlementRoutes.svelte from template

---
+++ src/EntitlementRoutes.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Route, MasterRoute } from "svelte-guard-history-router";
+  import { Entitlement } from "svelte-entitlement";
+  import EntitlementsPage from "./pages/EntitlementsPage.svelte";
+  import { fetchIterator } from "./util.mjs";
+
+  export let session;
+  export let guards;
+</script>
+
+<Route
+  path="/entitlement"
+  factory={MasterRoute}
+  iteratorFor={transition => fetchIterator('/entitlement', Entitlement, session)}
+  objectInstance={Entitlement}
+  component={EntitlementsPage}
+  {guards}>
+  <slot />
+</Route>
