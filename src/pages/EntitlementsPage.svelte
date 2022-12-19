23cd930c72a4cee024234622d0cd46f21844df88
Markus Felten <Markus.Felten@gmx.de>
2022-12-19 19:09:36 +0100

    add missing src/pages/EntitlementsPage.svelte from template

---
+++ src/pages/EntitlementsPage.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { EntitlementBadges } from "svelte-entitlement";
+
+  export let router;
+
+  const route = router.route;
+  const entitlements = $route.value;
+</script>
+
+<EntitlementBadges {entitlements} />
