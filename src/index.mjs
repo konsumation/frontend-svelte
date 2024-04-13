3be93edddb29d5d8ab1b253b18bac74066a95619
Markus Felten <Markus.Felten@gmx.de>
2024-04-13 10:19:35 +0200

    fix: cleanup

--- src/index.mjs
+++
@@ -1,8 +0,0 @@
-import pacote from "pacote";
-
-export async function npmDepAnalyser(name, version, categorizer) {
-  const pkg = await pacote.manifest(`${name}@${version}`);
-  console.log(pkg);
-}
-
-npmDepAnalyser("pacote","^1");
