3d99e059b101429c4b70c91b42c02bd8a593ead7
Markus Felten <Markus.Felten@gmx.de>
2024-05-15 21:51:59 +0100

    fix: switch to svelte@next

--- src/main.mjs
+++ src/main.mjs
@@ -1,12 +1,11 @@
+import { mount } from 'svelte';
 import App from "./App.svelte";
+
+const app = mount(App, { target: document.body});
+
+export default app;
 
 const worker = new Worker(
   new URL("./service-worker/main.mjs", import.meta.url),
   { type: "module" }
 );
-
-console.log(worker);
-
-export default new App({
-  target: document.body
-});
