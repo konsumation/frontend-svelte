548888fcd98f29d992de5ac3aeca59da31e233d3
Markus Felten <Markus.Felten@gmx.de>
2024-07-15 14:12:44 +0100

    fix: remove worker

--- src/main.mjs
+++ src/main.mjs
@@ -4,8 +4,3 @@ import App from "./App.svelte";
 const app = mount(App, { target: document.body});
 
 export default app;
-
-const worker = new Worker(
-  new URL("./service-worker/main.mjs", import.meta.url),
-  { type: "module" }
-);
